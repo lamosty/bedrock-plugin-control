8bb84abcfc4b60df15e563d0589086738dbe7881
Rastislav Lamos <lamos.rasto@gmail.com>
2015-04-03 18:06:55 +0200

    Plugin revamped

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+### 1.0.0: 2015-04-03
+
+* Plugin completely revamped
+* Activation/deactivation hooks working properly (using WordPress native functions to activate/deactivate plugins
--- bedrock-plugin-control.php
+++ bedrock-plugin-control.php
@@ -1,134 +1,68 @@
 <?php namespace Lamosty\Bedrock_Plugin_Control;
 /*
 Plugin Name: Bedrock plugin control
-Description: Force-enables or force-disables plugins specified in config/application.php. Based on Mark Jaquith's code.
-Version: 0.1.0
+Description: Force-enables or force-disables plugins specified in config/application.php.
+Version: 1.0.0
 License: GPL version 2 or any later version
 Author: Rastislav Lamos
 Author URI: https://lamosty.com/
 */
 
-class Disable_Plugins {
-	protected $plugins = array();
-	protected $message = 'Disabled in this environment';
+class Bedrock_Plugin_Control {
+	protected $plugins = [ ];
+	protected $wp_env;
 
-	/**
-	 * Sets up the options filter, and optionally handles an array of plugins to disable
-	 *
-	 * @param array $disables Optional array of plugin filenames to disable
-	 */
-	public function __construct( Array $plugins, $message = null ) {
-		// Handle what was passed in
-		foreach ( $plugins as $plugin ) {
-			$this->choose( $plugin );
-		}
+	protected function __construct( array $plugins ) {
+		$this->plugins = $plugins;
+		$this->wp_env = WP_ENV;
 
-		if ( ! is_null( $message ) ) {
-			$this->message = $message;
-		}
-
-		// Add the filter
-		add_filter( 'option_active_plugins', array( $this, 'alter' ) );
+		$this->handle_dev_plugins();
 	}
 
-	/**
-	 * Adds a filename to the list of plugins to disable
-	 */
-	public function choose( $file ) {
-		$this->plugins[] = $file;
-		add_filter( 'plugin_action_links_' . plugin_basename( $file ), array( $this, 'change_action_links' ) );
-	}
+	public static function init( array $plugins = [ ] ) {
+		static $instance = null;
 
-	function change_action_links( $actions ) {
-		unset( $actions['activate'] );
-		unset( $actions['delete'] );
-		$actions['disabled'] = '<i>' . esc_html( $this->message ) . '</i>';
+		if ( ! $instance ) {
 
-		return $actions;
-	}
-
-	/**
-	 * Hooks in to the option_active_plugins filter and does the disabling
-	 *
-	 * @param array $plugins WP-provided list of plugin filenames
-	 *
-	 * @return array The filtered array of plugin filenames
-	 */
-	public function alter( $plugins ) {
-		if ( count( $this->plugins ) ) {
-			foreach ( (array) $this->plugins as $plugin ) {
-				$key = array_search( $plugin, $plugins );
-				if ( false !== $key ) {
-					do_action( 'deactivate_plugin', $plugin );
-					unset( $plugins[ $key ] );
-				}
+			if ( empty( $plugins ) || ! defined( 'WP_ENV' ) ) {
+				return false;
 			}
+
+			$instance = new Bedrock_Plugin_Control( $plugins );
 		}
 
-		return $plugins;
-	}
-}
-
-class Enable_Plugins extends Disable_Plugins {
-	protected $message = 'Force-enabled';
-
-	function change_action_links( $actions ) {
-		unset( $actions['deactivate'] );
-		unset( $actions['delete'] );
-		$actions['enabled'] = '<i>' . esc_html( $this->message ) . '</i>';
-
-		return $actions;
+		return $instance;
 	}
 
-	/**
-	 * Hooks in to the option_active_plugins filter and does the enabling
-	 *
-	 * @param array $plugins WP-provided list of plugin filenames
-	 *
-	 * @return array The filtered array of plugin filenames
-	 */
-	public function alter( $plugins ) {
-		if ( count( $this->plugins ) ) {
-			foreach ( (array) $this->plugins as $plugin ) {
-				$key = array_search( $plugin, $plugins );
-				if ( false === $key ) {
-					do_action( 'activate_plugin', $plugin );
-					$plugins[] = $plugin;
-				}
+	private function handle_dev_plugins() {
+		require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+		if ( $this->wp_env == 'development' ) {
+			foreach ( $this->plugins as $plugin ) {
+				activate_plugin( $plugin );
 			}
+
+		} elseif ( $this->wp_env == 'production' ) {
+			deactivate_plugins( $this->plugins );
 		}
-
-		return $plugins;
 	}
 }
 
-function handle_bedrock_dev_plugins() {
-	/**
-	 * Add this variable to config/application.php.
-	 *
-	 * Plugins which get force-enabled in development environment.
-	 * Include them in your composer.json "require-dev" so they get installed only on dev machine.
-	 *
-	 * Specify relative path to plugin's main PHP file.
-	 *
-	 * Example:
-	 *
-	 * $BEDROCK_DEV_PLUGINS = array(
-	 * 'query-monitor/query-monitor.php',
-	 * 'debug-bar-console/debug-bar-console.php');
-	 */
-	global $BEDROCK_DEV_PLUGINS;
+/**
+ * Add this variable to config/application.php.
+ *
+ * Plugins which get force-enabled in development environment.
+ * Include them in your composer.json "require-dev" so they get installed only on dev machine.
+ *
+ * Specify relative path to plugin's main PHP file.
+ *
+ * Example:
+ *
+ * $BEDROCK_DEV_PLUGINS = array(
+ * 'query-monitor/query-monitor.php',
+ * 'debug-bar-console/debug-bar-console.php');
+ */
 
-	if ( ! defined( 'WP_ENV' ) || empty( $BEDROCK_DEV_PLUGINS ) ) {
-		return;
-	}
+global $BEDROCK_DEV_PLUGINS;
 
-	if ( WP_ENV == 'development' ) {
-		new Enable_Plugins( $BEDROCK_DEV_PLUGINS, 'Enabled for development' );
-
-	} elseif ( WP_ENV == 'production' ) {
-		new Disable_Plugins( $BEDROCK_DEV_PLUGINS, 'Enabled for development' );
-	}
-}
-
-handle_bedrock_dev_plugins();
+Bedrock_Plugin_Control::init( $BEDROCK_DEV_PLUGINS );
